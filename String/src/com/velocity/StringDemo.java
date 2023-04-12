0d41b9ce7ccb9a40e8acde40f2b1d990c56e258c
Madhurimagaikwad <madhurimag1996@gmail.com>
2023-04-12 22:30:04 +0530

    add string file

---
+++ String/src/com/velocity/StringDemo.java
@@ -0,0 +1,9 @@
+package com.velocity;
+
+public class StringDemo {
+ public static void main(String[] args) {
+	String s1="Madhurima";
+	String s=s1.concat("Gaikwad");
+	System.out.println(s);
+}
+}
